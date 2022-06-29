ef6949823c1021c640bef57b529bdd0d4caad6f7
gauravgjadhav <92154289+gauravgjadhav@users.noreply.github.com>
2022-06-30 00:00:50 +0530

    Add files via upload

---
+++ ConfigOopApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+class ConfigOopApp
+{
+    public static void Main(string[] args)
+    {
+        ConfigOopApp app = new ConfigOopApp();
+        Console.WriteLine("Connection1 information is : "+app.Connection1()+"\n Connection2 information is : "+app.Connection2());
+    }
+    public string Connection1()
+    {
+        string con = ConfigurationManager.AppSettings["connection1"];
+        return con;
+    }
+    public string Connection2()
+    {
+        string con1 = ConfigurationManager.AppSettings["connection2"];
+        return con1;
+    }
+}
---
+++ ConfligFileDemoApp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Configuration;
+class program
+{
+    public static void Main(string[] args)
+    {
+        string v1 = ConfigurationManager.AppSettings["k1"];
+        string v2 = ConfigurationManager.AppSettings["k2"];
+        string v3 = ConfigurationManager.AppSettings["k3"];
+        string v4 = ConfigurationManager.AppSettings["k4"];
+        Console.WriteLine(v1+" "+v2+" "+v3+" "+v4);
+    }
+}
